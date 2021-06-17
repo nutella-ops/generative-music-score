c35f162d5f5618a5765cb1bc19cc0eec04f5b9a3
higg <higginshiggins2@protonmail.com>
2021-06-17 12:41:15 -0700

    successfully iterates an output voltage from 0 to 255 using analogRead()

--- generative-music-score.ino
+++ generative-music-score.ino
@@ -1,21 +1,13 @@
-float aSection[] = {random(10, 40)/10.0, random(10, 40)/10.0, random(10, 40)/10.0, random(10, 40)/10.0};
-float bSection[] = {random(10, 40)/10.0, random(10, 40)/10.0, random(10, 40)/10.0, random(10, 40)/10.0};
-int i;
+int voltage;
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(115200);
-randomSeed(analogRead(0));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (i = 0; i < 4; i++) {
-  analogWrite(5, i);
-  Serial.print(String(i));
-  Serial.print(String(", "));
-  Serial.print(String(analogRead(5)));
-  Serial.println(String(""));
-  delay(1);
+for (voltage = 0; voltage < 255; voltage++) {
+  analogWrite(5, voltage);
+  delay(100);
   }
 }
