7d80900b1f9872fb04f321ef114232d6fb1e4cb5
higg <higginshiggins2@protonmail.com>
2021-06-18 12:13:15 -0700

    sno LED flash as expected, but seems like should work?

--- generative-music-score.ino
+++ generative-music-score.ino
@@ -1,18 +1,37 @@
-int voltage;
-int cd4066 = 13;
-int oscCtrl = 5;
+int baseOctave[12];
+int aSection[4];
+int bSection[4];
+int i;
+
 
 void setup() {
   // put your setup code here, to run once:
+pinMode(13, OUTPUT);
+Serial.begin(115200);
+randomSeed(analogRead(0));
+int baseOctave[] = {110, 116, 123, 130, 138, 146, 155, 164, 174, 184, 195, 207};
 }
 
-void loop() {
+int main() {
   // put your main code here, to run repeatedly:
-for (voltage = 95; voltage < 255; voltage++) {
-  digitalWrite(cd4066, HIGH);
-  analogWrite(oscCtrl, voltage);
-  delay(3000);
-  digitalWrite(cd4066, LOW);
-  delay(3000);
+  for (int i = 0; i < sizeof(baseOctave); i++) {
+    // (pin, hz, duration)
+    tone(13, baseOctave[i], 50);
+    delay(200);
   }
 }
+
+
+// base frequency range [unsigned integer]
+// 207						
+// 195
+// 184				
+// 174						
+// 164						
+// 155						
+// 146
+// 138						
+// 130			
+// 123						
+// 116						
+// 110
