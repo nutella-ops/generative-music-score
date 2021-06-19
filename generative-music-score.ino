64869c94e4d61022590116a8d8e839d40e33c198
higg <higginshiggins2@protonmail.com>
2021-06-18 20:40:47 -0700

    tried using constants from pitches.h instead of baseOctave, but same performance error

--- generative-music-score.ino
+++ generative-music-score.ino
@@ -1,28 +1,30 @@
-int baseOctave[12];
-int aSection[4];
-int bSection[4];
-int i;
+#include "pitches.h"
 
+// base frequency range
+int baseOctave[12] = {110, 116, 123, 130, 138, 146, 155, 164, 174, 184, 195, 207};
+
+// alternate baseOctave using library constants 
+int altOctave[12] = {NOTE_A2, NOTE_AS2, NOTE_B2, NOTE_C3, NOTE_CS3, NOTE_D3, NOTE_DS3, NOTE_E3, NOTE_F3, NOTE_FS3, NOTE_G3, NOTE_GS3};
+int i;
+int duration = 2000;
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(13, OUTPUT);
-Serial.begin(115200);
-randomSeed(analogRead(0));
-int baseOctave[] = {110, 116, 123, 130, 138, 146, 155, 164, 174, 184, 195, 207};
+  pinMode(13, OUTPUT);
+  Serial.begin(115200);
+  randomSeed(analogRead(0));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   for (int i = 0; i < sizeof(baseOctave); i++) {
     // (pin, hz, duration)
-    tone(13, 400, 100);
-    delay(500);
+    tone(13, altOctave[i], duration);
+    // silence
+    delay(duration); 
   }
 }
 
 
-// base frequency range [unsigned integer]
+// [unsigned integer]
 // 207						
 // 195
 // 184				
