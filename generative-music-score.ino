e8878a7e5419a2a3c32fedcae46fe92d93708952
higg <higginshiggins2@protonmail.com>
2021-06-18 22:33:54 -0700

    successfully chooses notes from baseOctave

--- generative-music-score.ino
+++ generative-music-score.ino
@@ -1,6 +1,6 @@
 int a, b, c, d, e, f, g, h;
 
-int a_elements[4] = {a, b, c, d};
+int a_elements[4];
 int a_size = sizeof(a_elements);
 int a_duration = 700;
 
@@ -21,25 +21,31 @@ void setup() {
   pinMode(ioPin, OUTPUT);
   Serial.begin(115200);
   randomSeed(analogRead(0));
-  
-  for (i = 0; i < sizeof(a_elements); i++) {
-    a_elements[i] = baseOctave[base_rand];
-    b_elements[i] = baseOctave[base_rand];
-  }
+ 
 }
 
 // tone(pin, hz, duration)
 void loop() {
+  int a_elements[4] = {baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)]};
+  Serial.println(String(a_elements[0]));
+  Serial.println(String(a_elements[1]));
+  Serial.println(String(a_elements[2]));
+  Serial.println(String(a_elements[3]));
+  Serial.println(String(""));
+
+
+
+
   for (int i = 0; i < sizeof(a_size); i++) {
-    tone(ioPin, a_elements[i], a_duration);
+    tone(ioPin, baseOctave[random(12)], a_duration);
     delay(a_duration);
+    Serial.println(String(a_elements[i]));
+    Serial.println(String(""));
   }
 
-  for (int i = 0; i < sizeof(b_size); i++) {
-    tone(ioPin, b_elements[i], b_duration);
-    delay(b_duration);
-  }
-    Serial.println(String(a_elements[i]));
-    Serial.println(String(b_elements[i]));
-    Serial.println(String(""));
+  // for (int i = 0; i < sizeof(b_size); i++) {
+  //   tone(ioPin, b_elements[i], b_duration);
+  //   delay(b_duration);
+  // }
+  
 }
