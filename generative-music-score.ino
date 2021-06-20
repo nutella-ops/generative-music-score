fdb7d18cce850ac32030bebd08040b245bc855fa
higg <higginshiggins2@protonmail.com>
2021-06-19 17:28:36 -0700

    permanently stuck on 4 chosen notes, even after reset button

--- generative-music-score.ino
+++ generative-music-score.ino
@@ -1,43 +1,35 @@
-int a, b, c, d, e, f, g, h;
-
-int a_elements[4];
-int a_size = sizeof(a_elements);
-int a_duration = 90;
-
-int b_elements[4] = {e, f, g, h};
-int b_size = sizeof(b_elements);
-int b_duration = 2500;
-
 int baseOctave[12] = {110, 116, 123, 130, 138, 146, 155, 164, 174, 184, 195, 207};
-int base_size = sizeof(baseOctave);
-int base_rand = random(base_size);
+
+int a_elements[4] = {baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)]};
+int a_size = sizeof(a_elements) / sizeof(int); // this was main bug affection functionality
+int a_duration = 2500;
+
+int b_elements[4] = {baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)]};
+int b_size = sizeof(b_elements) / sizeof(int); // this was main bug affection functionality
+int b_duration = 2500;
 
 int i;
 int ioPin = 13;
-
 
 
 void setup() {
   pinMode(ioPin, OUTPUT);
   Serial.begin(115200);
   randomSeed(analogRead(0));
- 
 }
 
-// tone(pin, hz, duration)
-void loop() {
-  int a_elements[4] = {baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)]};
-  
-  for (int i = 0; i < sizeof(a_size); i++) {
-    tone(ioPin, baseOctave[random(12)], a_duration);
-    delay(random(25, 450));
+
+void loop() { 
+  for (int i = 0; i < a_size ;i++) {
     Serial.println(String(a_elements[i]));
-    Serial.println(String(""));
+  } 
+  for (int i = 0; i < a_size; i++) {
+    tone(ioPin, a_elements[i], a_duration); // tone(Pin, Hz, Duration)
+    delay(a_duration);
   }
-
-  // for (int i = 0; i < sizeof(b_size); i++) {
-  //   tone(ioPin, b_elements[i], b_duration);
-  //   delay(b_duration);
-  // }
-  
+  Serial.println(String(""));
 }
+
+// sizeof() in C returns the size of the variable in bytes, not the length of the array. 
+//   So you have to divide by the size of the contained type to get the length.
+//   int a_size = sizeof(a_elements) / sizeof(int);
