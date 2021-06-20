a2285c838eb25cca76cc7242e292a509827428ba
higg <higginshiggins2@protonmail.com>
2021-06-20 00:12:09 -0700

    workaround by nested while(1) and added fixed length assignments to notes

--- generative-music-score.ino
+++ generative-music-score.ino
@@ -1,7 +1,6 @@
 int baseOctave[12] = {110, 116, 123, 130, 138, 146, 155, 164, 174, 184, 195, 207};
 
-int a_elements[4] = {baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)]};
-int a_size = sizeof(a_elements) / sizeof(int); // this was main bug affection functionality
+
 
 int b_elements[4] = {baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)]};
 int b_size = sizeof(b_elements) / sizeof(int); // this was main bug affection functionality
@@ -20,16 +19,29 @@ void setup() {
 
 
 void loop() {
+  int a_elements[4] = {baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)], baseOctave[random(12)]};
+  int a_size = sizeof(a_elements) / sizeof(int); // this was main bug affection functionality
+  int a_note[4] = {note[random(5)], note[random(5)], note[random(5)], note[random(5)]};
+  int b_note[4] = {note[random(5)], note[random(5)], note[random(5)], note[random(5)]};
+
+  Serial.println(String(""));
   for (int i = 0; i < a_size; i++) {
-    int note_length = note[random(5)];
-    tone(ioPin, a_elements[i], note_length); // tone(Pin, Hz, Duration)
-    delay(note_length); // silence
+    Serial.println(String(a_elements[i]));
+    Serial.println(String(a_note[i]));
   }
 
-  for (int i = 0; i < b_size; i++) {
-    int note_length = note[random(5)];
-    tone(ioPin, b_elements[i], note_length); // tone(Pin, Hz, Duration)
-    delay(note_length);  // silence
+  while(1) {
+    for (int i = 0; i < a_size; i++) {
+      // int note_length = note[random(5)];
+      tone(ioPin, a_elements[i], a_note[i]); // tone(Pin, Hz, Duration)
+      delay(b_note[i]); // silence
+    }
+
+    for (int i = 0; i < b_size; i++) {
+      // int note_length = note[random(5)];
+      tone(ioPin, b_elements[i], b_note[i]); // tone(Pin, Hz, Duration)
+      delay(b_note[i]);  // silence
+    }
   }
 }
 
